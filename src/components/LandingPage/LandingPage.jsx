88a168fc11bbb0b261e8ccdf3b359e850c4c4284
jakatsa <jakatsa5@gmail.com>
2025-03-02 20:16:17 +0300

    removal of lazy loading

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,16 +1,12 @@
-import React, { lazy, Suspense } from "react";
+import React from "react";
+import { AllBeatsPage } from "../AllBeatsPage/AllBeatsPage";
 import { CategoryList } from "../CategoryList/CategoryList";
-
-// Lazy load AllBeatsPage
-const AllBeatsPage = lazy(() => import("../AllBeatsPage/AllBeatsPage"));
 
 export const LandingPage = () => {
   return (
     <div>
       <CategoryList />
-      <Suspense fallback={<div>Loading AllBeats...</div>}>
-        <AllBeatsPage />
-      </Suspense>
+      <AllBeatsPage />
     </div>
   );
 };
